71fbca0f8c261f4c6b4191a08955019ef9597e5c
davidcoats <david@getsafetycone.com>
2023-07-20 09:32:39 -0700

    Initial commit.

---
+++ source/R5T.S0088/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.S0088
+{
+	/// <summary>
+	/// Post-process instances to replace <inheritdoc> elements.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
